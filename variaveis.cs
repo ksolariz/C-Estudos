6c59f360d9554c3b2b630fac466a70251704358c
Kauê Soares <83794987+ksolariz@users.noreply.github.com>
2022-03-27 22:47:09 -0300

    Create variaveis.cs

---
+++ variaveis.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCompleto = "Kauê Soares Ferreira";
+            int idadePessoa = 18;
+            double salario = 2.755;
+            bool pessoaBonita = true;
+            Console.WriteLine("Aprendendo a crair variaveis com C#");
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(idadePessoa);
+            Console.WriteLine(salario);
+            Console.WriteLine(pessoaBonita);
+
+
+        }
+    }
+}
